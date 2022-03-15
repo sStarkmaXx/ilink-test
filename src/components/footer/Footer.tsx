adad1978ee15fb9dbef334e587503395d6ba9545
sStarkmaXx <60282152+sStarkmaXx@users.noreply.github.com>
2022-03-15 22:25:37 +0700

    1
    create app

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import css from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={css.footer}>
+      <div className={css.text}>© iLINK ACADEMY. ALL RIGHTS RESERVED. 2022</div>
+      <div className={css.contactsGroup}></div>
+    </div>
+  );
+};
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import css from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <div className={css.header}>
+      <div className={css.headerGroup}>
+        <div className={css.accountGroup}>
+          <div className={css.avatar}></div>
+          <div className={css.accountName}>Max Myasnikov</div>
+        </div>
+        <div className={css.academyName}>ILINK academy</div>
+        <button className={css.headerButton}>Панель управления</button>
+      </div>
+    </div>
+  );
+};
